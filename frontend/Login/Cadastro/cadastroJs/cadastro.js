b59baa055c8957e34d6718a5259bf8445bfab654
benetesla <78994881+benetesla@users.noreply.github.com>
2022-11-09 17:36:09 -0300

    Criacao das paginas de login , sistema cadastro

---
+++ frontend/Login/Cadastro/cadastroJs/cadastro.js
@@ -0,0 +1,32 @@
+let senha = document.getElementById("senha");
+let confirmarSenha = document.getElementById("confirmarSenha");
+let cadastrar = document.getElementById("cadastrar");
+confirmarSenha.addEventListener("keyup", function() {
+    if (senha.value != confirmarSenha.value) {
+        confirmarSenha.setCustomValidity("Senhas diferentes");
+    } else {
+        confirmarSenha.setCustomValidity("");
+    }
+});
+//estamos verificando a idade do usuario, se for menor de 18 anos, ele não pode se cadastrar 
+let idade = document.getElementById('idade');
+let cadastra = document.getElementById('cadastrar');
+cadastra.disabled = true;
+idade.addEventListener('keyup', function() {
+    if (idade.value >= 18) {
+        cadastra.disabled = false;
+    } else {
+        cadastra.disabled = true;
+    }
+});
+//verificar estamos
+let email = document.getElementById('email');
+let cadastro = document.getElementById('cadastro');
+cadastro.disabled = true;
+email.addEventListener('keyup', function() {
+    if (email.value.indexOf('@') != -1) {
+        cadastro.disabled = false;
+    } else {
+        cadastro.disabled = true;
+    }
+});
---
+++ frontend/Login/LoginJS/login.js
@@ -0,0 +1,1 @@
+alert("Bem vindo ao sistema de login");
