fc6d97f2c732c5c184f791305c96e44850fcb0e0
benetesla <78994881+benetesla@users.noreply.github.com>
2022-11-10 22:41:26 -0300

    Voltando os dados anteriores ao json

--- BB(FINALV01)/src/index.js
+++ BB(FINALV01)/src/index.js
@@ -3,6 +3,20 @@ const app = express();
 
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({
+        id: 1,
+        nome: "Bene",
+        idade: 25,
+        email: "bene369@gmail.com",
+        telefone: "11999999999",
+        endereco: "Rua dos bobos",
+        numero: 0,
+        bairro: "Centro",
+        cidade: "São Paulo",
+        estado: "SP",
+        cep: "00000000",
+        complemento: "Casa",
+        status: "Ativo"
+    });
 });
 app.listen(666, () => console.log('listening at 666'));
