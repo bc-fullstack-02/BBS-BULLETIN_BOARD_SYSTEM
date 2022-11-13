51e57384bd248e72a01b39d7a40a11640db2e3f7
benetesla <78994881+benetesla@users.noreply.github.com>
2022-11-13 11:06:13 -0300

    transfomando o menu em uma constante

--- BB(FINALV01)/frontend/bene-tesla/src/App.js
+++ BB(FINALV01)/frontend/bene-tesla/src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
 
 function App() {
+  const menu = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'About',
+      link: '/about',
+    },
+    {
+      name: 'Contact',
+      link: '/contact',
+    },
+  ];
   return (
+     
     <div className="App">
       <header className="App-header">
         <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Contact</a></li>
-
+          {menu.map((item) => (
+            <li>
+              <a href={item.link}>{
+                item.name
+              }</a>
+            </li>
+          ))}
+          
         </ul>
       </header>
+        
     </div>
+   
   );
 }
 
