4b44c78645fd3320a7c09fc1326e6d1d1fd8ed3b
benetesla <78994881+benetesla@users.noreply.github.com>
2022-11-15 09:44:04 -0300

    adicinando evento em um formulario

--- BB(FINALV01)/ExerciosFront.Back/bene-tesla/src/components/eventos.js
+++ BB(FINALV01)/ExerciosFront.Back/bene-tesla/src/components/eventos.js
@@ -1,12 +1,36 @@
 function eventos(){
     return(
         <div>
-            <h1>Eventos</h1>
-            <button onClick={click}>Clique aqui</button>
+            <form action="">
+                <input type="text" name="nome" id="nome" placeholder="Nome"/>
+                <input type="text" name="idade" id="idade" placeholder="Idade"/>
+                <input type="text" name="cidade" id="cidade" placeholder="Cidade"/>
+                <input type="text" name="estado" id="estado" placeholder="Estado"/>
+                <input type="text" name="pais" id="pais" placeholder="Pais"/>
+                <input type="text" name="id" id="id" placeholder="ID"/>
+                <button click="cadastrar()">Cadastrar</button>
+            </form>
+           
         </div>
+    
     )
 }
-function click(){
-    alert("Clicou")
-}
+function cadastrar(){
+    const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
+    const cidade = document.getElementById("cidade").value;
+    const estado = document.getElementById("estado").value;
+    const pais = document.getElementById("pais").value;
+    const id = document.getElementById("id").value;
+    const pessoa = {
+        nome: nome,
+        idade: idade,
+        cidade: cidade,
+        estado: estado,
+        pais: pais,
+        id: id
+    }
+    console.log(pessoa);
+}  
+
 export default eventos;
