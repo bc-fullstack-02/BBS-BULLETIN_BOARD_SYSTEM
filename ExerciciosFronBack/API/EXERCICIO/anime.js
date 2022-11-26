73ecee51f5d65a4f08972f544077181760a5d43e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-11-26 09:02:48 -0300

    Exlusao de algumas dependencias

--- ExerciciosFronBack/API/EXERCICIO/anime.js
+++ ExerciciosFronBack/API/EXERCICIO/anime.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const express = require('express');
-const app = express();
 const port = 3000;
-const server = http.createServer(app);
-
+const server = http.createServer();
+/*
+inicianco o servidor
+*/
 server.listen(port);
