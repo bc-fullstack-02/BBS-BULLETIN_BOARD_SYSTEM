951a437cb63197aa1f2763f1f79b8c02dc6d86b9
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-11-30 11:32:47 -0300

    Iniciando com o git

--- ExerciciosFronBack/API/EXERCICIO/anime.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-const lancamentos = require('./lancamento');
-const server = http.createServer(lancamentos);
-
-server.listen(port);
