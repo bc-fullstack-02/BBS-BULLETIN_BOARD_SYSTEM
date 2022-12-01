07b1adf958ffac84e7bb5658342db26ad38de0ab
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-01 18:40:58 -0300

    Continuando as alteracoes na HOME

--- ExerciciosFronBack/meuapp/meuapp/src/pages/Inicio/home.js
+++ ExerciciosFronBack/meuapp/meuapp/src/pages/Inicio/home.js
@@ -8,54 +8,47 @@ function Home() {
           <img src={logo} alt=" Logo da nossa aplicacao" />
           <h1>B-B-S</h1>
           <button>LOGIN</button>
-          
+
         </div>
       </nav>
       <section className="input-section">
-    <form>
-      <h1>PESQUISAR</h1>
-      <div className="input-group">
+        <form>
+          <h1>PESQUISAR</h1>
+          <div className="input-group">
             <label user="users">Usuarios :</label>
             <input type="text" id="users" placeholder="Nome do usuario" />
             <label user="repos">Repositorios :</label>
             <input type="text" id="repos" placeholder="Nome do repositorio" />
-      </div>
-    </form>
+          </div>
+        </form>
       </section>
       <section className="Users">
-        <div className="card">
-          <h2>NOME USUARIO</h2>
-          <h3>Repositorios</h3>
-          <div className="information">
-            <img src={""} alt="" />
-            <p>Contato</p>
-          </div>    
-        </div>
-        <div className="card">
-          <h2>NOME USUARIO</h2>
-          <h3>Repositorios</h3>
-          <div className="information">
-            <img src={""} alt="" />
-            <p>Contato</p>
+        <div className="UsuariosContainer">
+          <div className="card">
+            <h2>NOME USUARIO</h2>
+            <h3>Repositorios</h3>
+            <div className="information">
+              <img src={""} alt="" />
+              <p>Contato</p>
+            </div>
+          </div>
+          <div className="card">
+            <h2>NOME USUARIO</h2>
+            <h3>Repositorios</h3>
+            <div className="information">
+              <img src={""} alt="" />
+              <p>Contato</p>
+            </div>
+          </div>
+          <div className="card">
+            <h2>NOME USUARIO</h2>
+            <h3>Repositorios</h3>
+            <div className="information">
+              <img src={""} alt="" />
+              <p>Contato</p>
+            </div>
           </div>
         </div>
-        <div className="card">
-          <h2>NOME USUARIO</h2>
-          <h3>Repositorios</h3>
-          <div className="information">
-            <img src={""} alt="" />
-            <p>Contato</p>
-          </div>
-        </div>
-        <div className="card">
-          <h2>NOME USUARIO</h2>
-          <h3>Repositorios</h3>
-          <div className="information">
-            <img src={""} alt="" />
-            <p>Contato</p>
-          </div>
-        </div>
-      
       </section>
     </>
 
