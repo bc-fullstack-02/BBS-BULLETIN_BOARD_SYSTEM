adc08d082ff02016ca2e2517062b59014ad44a31
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-02 16:25:04 -0300

    Alteracoes do INPUT

--- ExerciciosFronBack/meuapp/meuapp/src/Component/Cadastrar/cadastrar.js
+++ ExerciciosFronBack/meuapp/meuapp/src/Component/Cadastrar/cadastrar.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Cadastrar() {
     return (
         <div className="Background">
             <div className="modal">
-                <h1>CADASTRAR</h1>
+                <h1>CRIAR UMA NOVA CONTA</h1>
+                
                 <form>
                     <input type="text" name="name" id="name" placeholder="Digite seu nome" required />
+                    <input type="text" name="Sobrenome" placeholder="Digite seu sobrenome" required />
                     <input type="text" name="email" id="email" placeholder="Digite seu email" required />
                     <input type="password" name="password" id="password" placeholder="Digite sua senha" required />
+                    <label>DATA DE NASCIMENTO</label>
+                    <input type="date" name="date" id="date" required />
+                    <label>SEXO</label>
+                    <select name="sexo" id="sexo">
+                        <option value="masculino">Masculino</option>
+                        <option value="feminino">Feminino</option>
+                        <option value="outro">Outro</option>
+                    </select>
+
                     <button>CADASTRAR</button><br></br>
+                    <Link>JA TENHO CONTA</Link>
                 </form>
             </div>
         </div>
