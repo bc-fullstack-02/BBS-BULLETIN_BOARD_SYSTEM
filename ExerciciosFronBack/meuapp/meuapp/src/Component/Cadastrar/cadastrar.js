1da8e8176441f778e3ee552cb0e31155ca2da5b4
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-02 16:00:18 -0300

    Criacao da pagina de cadastro

--- ExerciciosFronBack/meuapp/meuapp/src/Component/Cadastrar/cadastrar.js
+++ ExerciciosFronBack/meuapp/meuapp/src/Component/Cadastrar/cadastrar.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-function Login() {
+function Cadastrar() {
     return (
-
         <div className="Background">
-
             <div className="modal">
-
-                <h1>ENTRAR</h1>
+                <h1>CADASTRAR</h1>
                 <form>
                     <input type="text" name="name" id="name" placeholder="Digite seu nome" required />
                     <input type="text" name="email" id="email" placeholder="Digite seu email" required />
                     <input type="password" name="password" id="password" placeholder="Digite sua senha" required />
-                    <button>ENTRAR</button><br></br>
-                    <div className="Cadastro">
-                        <Link>Criar conta</Link>
-                    </div>
+                    <button>CADASTRAR</button><br></br>
                 </form>
             </div>
         </div>
     )
 }
-export default Login;
+export default Cadastrar;
