50aeb65d82bb4d584db09b4b91b906400f1bd01c
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-02 19:54:49 -0300

    Modal:alteracao da visibilidade do modal

--- ExerciciosFronBack/meuapp/meuapp/src/pages/Inicio/home.js
+++ ExerciciosFronBack/meuapp/meuapp/src/pages/Inicio/home.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Nav from "../../Component/nave/navbar";
 import CardUser from "../../Component/CardUSER/usercard";
 import Input from "../../Component/Input/input";
 import ModalControllerLogin from "../../Component/MODALCONTOLLERLOGIN/modal";
 function Home() {
+/*esconder e mostrar o modal*/
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
       <Nav />
       <Input />
       <CardUser />
-      <ModalControllerLogin />
-
+      
+    {isModalOpen ? <ModalControllerLogin /> : null}
     </>
   );
 }
