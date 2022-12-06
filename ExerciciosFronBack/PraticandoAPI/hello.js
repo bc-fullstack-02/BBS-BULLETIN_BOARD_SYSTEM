b90632f6e1ca5fbbff845332be5ad1bbe6f81e25
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-06 08:27:26 -0300

    JSON: Formantando o JSON com Spaces

--- ExerciciosFronBack/PraticandoAPI/hello.js
+++ ExerciciosFronBack/PraticandoAPI/hello.js
@@ -1,1 +1,23 @@
-console.log("Tamo praticando API");
+import  Express  from "express";
+const app = Express();
+const port = 3008;
+
+app.use(Express.json());
+app.set("json spaces", 5);
+app.get("/", (req, res) => {
+    res.json({ 
+        "name": "bene",
+        "lastname": "silva",
+        "age": 25,
+        "city": "Nova Odessa",
+        "state": "SP",
+        "country": "Brasil",
+        "id": 1
+
+     });
+    
+}
+);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
