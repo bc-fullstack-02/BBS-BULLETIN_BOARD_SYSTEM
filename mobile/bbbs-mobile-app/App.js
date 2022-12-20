0a34d1e4ecc5c2a60933988d350c55b308f3a842
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-20 11:48:06 -0300

    MobileTestes

---
+++ mobile/bbbs-mobile-app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
