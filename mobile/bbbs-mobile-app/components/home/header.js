087dc747f642b39adfaa9366542c5818fa47b986
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2022-12-22 08:02:46 -0300

    MOBILE:ADD HEADER COMPONENT

--- mobile/bbbs-mobile-app/components/home/header.js
+++ mobile/bbbs-mobile-app/components/home/header.js
@@ -1,13 +1,32 @@
-import { View, Text , StyleSheet, TouchableOpacity} from 'react-native'
+import { View, StyleSheet,Text, TouchableOpacity} from 'react-native'
 import { Image } from 'react-native'
 import React from 'react'
 
 const header = () => {
   return (
-    <View styek>
+    <View style={estilos.container}>
     <TouchableOpacity>
          <Image style={estilos.logo} source={require('../../assets/images/logo.png')} />
     </TouchableOpacity>
+
+    <View style={estilos.iconScreeen}>
+        <TouchableOpacity>
+            <Image style={estilos.icon} source={require('../../assets/images/dark.png')} />
+        </TouchableOpacity>
+        
+        <TouchableOpacity>
+            <Image style={estilos.icon} source={require('../../assets/images/noti.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Image style={estilos.icon} source={require('../../assets/images/exc.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={estilos.unread}>
+            <Text style={estilos.unreadText}>250</Text>
+          </View>
+            <Image style={estilos.icon} source={require('../../assets/images/mensage.png')} />
+        </TouchableOpacity>
+    </View>
     </View>
   )
 }
@@ -18,8 +37,43 @@ const estilos = StyleSheet.create({
        resizeMode: 'contain',
         marginTop: 26,
         borderRadius: 10,
+    },
+    container:{
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        marginHorizontal: 20,
+    },
+    iconScreeen:{
+        flexDirection: 'row',
+    },
+    icon:{
+        padding: 10,
+        marginTop: 26,
+        width: 15,
+        height: 25,
+        marginLeft: 10,
+        resizeMode: 'contain',      
+    },
+    unread:{
+        backgroundColor: '#FF0000',
+        position: 'absolute',
+        left: 20,
+        top: 19,
+        bottom:18,
+        width: 25,
+        height: 14,
+        borderRadius: 10,
+        justifyContent: 'center',
+        zIndex: 200,
+    },
+    unreadText:{
+        color: '#fff',
+        fontWeight: '600',
+        fontSize: 10,
+        textAlign: 'center',
+        
     }
 })
-
-
 export default header;
